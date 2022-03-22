9f076f82dad30e5b5a23c9403d33599ff05d66ee
Nick Huemmer <njhuemmer@gmail.com>
2022-03-22 11:42:32 -0400

    begin building React app

--- portfolio_2022/src/App.js
+++ portfolio_2022/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+//import app elements
+import Header from './components/Header';
+import Main from './components/Main';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Blog from './components/Blog';
+import NotFound from './components/NotFound';
+import UnhandledError from './components/UnhandledError';
+
+// App provides routing around the Portfolio site
+const App = () => (
+  <Router>
+      <div> 
+      <Header />
+      
+      <Routes>
+        <Route exact path="/" component={()=><Main/>} />
+        <Route path="/main" component={()=><Main/>} />
+        <Route path="/about" component={()=><About/>} />
+        <Route path="/contact" component={()=><Contact/>} />
+        <Route path="/portfolio" component={()=><Portfolio/>} />
+        <Route path="/resume" component={()=><Resume/>} />
+        <Route path="/blog" component={()=><Blog/>} />
+        <Route path='/error' component={UnhandledError} />
+        <Route  component={NotFound} />
+      </Routes>  
+      </div>
+    </Router>
+);
 
 export default App;
---
+++ portfolio_2022/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+
+    return (
+        <div className="name-title">
+            <h1 className="name">Nick Huemmer</h1>
+        </div>
+
+    )
+
+}
+
+export default Main;
